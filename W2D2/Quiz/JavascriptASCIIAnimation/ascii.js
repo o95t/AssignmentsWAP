8d4a08710fa49147147e684b580a5db86937d92c
o95t <omar_95tamimi@hotmail.com>
2023-05-10 04:17:04 -0500

    JavascriptASCIIAnimation

--- W2D2/Quiz/JavascriptASCIIAnimation/ascii.js
+++
@@ -1,9 +0,0 @@
-
-$("#start").on("click", function () {
-
-    var animation = $('#animation').val();
-    var size = $('#size').val();
-    // Log the selected value to the console
-    console.log(animation);
-    alert(size + animation + "  Handler for `click` called.");
-});
