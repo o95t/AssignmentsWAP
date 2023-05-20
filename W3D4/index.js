36e1e14d50aea5f1358f673808f5f9315d3c02de
o95t <omar_95tamimi@hotmail.com>
2023-05-19 22:28:20 -0500

    change JS

--- W3D4/index.js
+++ W3D4/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-
-const quiz = require('./quiz.js');
-
+const Questions = require('./questions.js');
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -26,9 +24,76 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    return res.render("index_view");
+    return res.render("index");
 });
 
-//both index.js and quiz.js should be in same directory
-app.use('/quiz', quiz);
+app.post('/quiz/q/:id', function (req, res) {
+    const id = req.params.id;
+    if (id >= 1 && id <= 4) {
+       req.session['q' + id] = new Number(req.body.answer);
+       const newId = Number(id) + 1;
+       printSession(req);
+       return res.redirect("/quiz/q/" + newId)
+       
+    }
+    if (id == 5) {
+       req.session.q5 = new Number(req.body.answer);
+       return res.redirect('/quiz/result');
+    }
+    else {
+       res.writeHead(404, { 'Content-Type': 'text/html' });
+       return res.end("404 Not Found");
+    }
+ });
+
+ app.get('/quiz/q/:id', function (req, res) {
+    const id = req.params.id;
+    if (id == 1) {
+       req.session.score = 0;
+       req.session.q1 = Number.NEGATIVE_INFINITY;
+       req.session.q2 = Number.NEGATIVE_INFINITY;
+       req.session.q3 = Number.NEGATIVE_INFINITY;
+       req.session.q4 = Number.NEGATIVE_INFINITY;
+       req.session.q5 = Number.NEGATIVE_INFINITY;
+    }
+ 
+    if (id >= 1 && id <= 5) {
+       return  res.render('q_view', {
+          score: calculateScore(req),
+          question: Questions.questions[(id - 1)],
+          id: req.params.id
+       });
+    }
+    else {
+       res.writeHead(404, { 'Content-Type': 'text/html' });
+       return res.end("404 Not Found");
+    }
+ 
+ });
+
+function printSession(req) {
+    const id = req.params.id;
+    console.log(req.session);
+    for (let i = 1; i <  id; i++) {
+        console.log(req.session['q'+i]);
+     }
+}
+
+
+app.get('/quiz/result', function (req, res) {
+    res.render('result_view', { score: calculateScore(req) });
+ });
+ 
+ function calculateScore(req) {
+    let score = 0;
+ 
+    for (let i = 0; i < 5; i++) {
+ 
+       if (req.session['q' + (i + 1)] == Questions.answers[i]) {
+          score++;
+       }
+    }
+    return score;
+ }
+
 app.listen(8080);
